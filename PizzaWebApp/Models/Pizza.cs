2247a9a959afb12b7a0a00c10884fc92bef13fd1
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-14 01:00:00 +0200

    Creating a Pizza Model

---
+++ PizzaWebApp/Models/Pizza.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaWebApp.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Ingredients { get; set; }
+    }
+}
