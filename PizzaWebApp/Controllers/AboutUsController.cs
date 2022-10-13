35c0791c408b6d7de38fbabe24a5c3887cd6e823
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-13 19:42:06 +0200

    Layout Changes

---
+++ PizzaWebApp/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaWebApp.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
