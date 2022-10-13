c28a09f8bb6837ca4a598bac32aa0bd55d7d2446
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-13 17:28:52 +0200

    Application DB Context File

---
+++ PizzaWebApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BorrowLanded.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
