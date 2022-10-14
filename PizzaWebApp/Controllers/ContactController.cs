394fbc8c7f85352ab83223d6dffde7bfc4b28fe8
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-14 03:35:39 +0200

    Create Contact Entry Controller

--- PizzaWebApp/Controllers/ContactController.cs
+++ PizzaWebApp/Controllers/ContactController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaWebApp.Data;
+using PizzaWebApp.Models;
 
 namespace PizzaWebApp.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public ContactController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create(Contact obj)
+        {
+            _db.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
