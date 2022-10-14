90aefa9eefa2251074553f9bf9845d462e82c0a8
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-14 14:34:45 +0200

    Booking Controller and View

--- PizzaWebApp/Controllers/BookingController.cs
+++ PizzaWebApp/Controllers/BookingController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaWebApp.Data;
+using PizzaWebApp.Models;
 
 namespace PizzaWebApp.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public BookingController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create(Bookings obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+                ViewBag.SuccessMessage = "Your booking was set successfully! You will be called to confirm your booking.";
+
+            }
+            return View(obj);
         }
     }
 }
