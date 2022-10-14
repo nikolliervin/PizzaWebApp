bace33e945099813845bf0b17d14e21d9e218bf9
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-14 03:18:57 +0200

    Contact Model

---
+++ PizzaWebApp/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace PizzaWebApp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
