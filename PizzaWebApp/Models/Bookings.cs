1f247a75f54346d70c37ca2e69e9b338af73f3e5
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-14 14:19:19 +0200

    Created Booking Model

---
+++ PizzaWebApp/Models/Bookings.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaWebApp.Models
+{
+    public class Bookings
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public int NrOfPeople { get; set; }
+
+        public string Message { get; set; }
+    }
+}
