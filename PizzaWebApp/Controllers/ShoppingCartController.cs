a2e382f05b772f423c11014facbdb66766c084af
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-11-01 20:52:46 +0100

    Setting up the shopping cart view

--- PizzaWebApp/Controllers/ShoppingCartController.cs
+++ PizzaWebApp/Controllers/ShoppingCartController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaWebApp.Data;
+using PizzaWebApp.Models;
+using System.Collections.Generic;
 
 namespace PizzaWebApp.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public ShoppingCartController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<CartItems> objList = _db.Cart;
+
+            return View(objList);
+
         }
     }
 }
