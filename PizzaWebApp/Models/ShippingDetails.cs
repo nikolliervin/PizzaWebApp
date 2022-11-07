1ab763da67844880d4c307bdf1a4bb223a5c2e4d
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-11-07 23:47:22 +0100

    Shipping Detals Model

---
+++ PizzaWebApp/Models/ShippingDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaWebApp.Models
+{
+    public class ShippingDetails
+    {
+        public int ShippingId { get; set; }
+
+        public string Street { get; set; }
+
+        public string Street2 { get; set; }
+
+        public string City { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
