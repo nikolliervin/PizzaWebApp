1fd47b5959206f9591170bf390d510d5a7c3e739
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-11-17 18:26:21 +0100

    Order confirm view

---
+++ PizzaWebApp/Controllers/Order.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaWebApp.Data;
+using System.Security.Claims;
+
+namespace PizzaWebApp.Controllers
+{
+    public class Order : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public Order(ApplicationDBContext data)
+        {
+            _db = data;
+        }
+        public IActionResult Index(int? id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            ViewBag.ShDetails = _db.ShippingDetails.Find(userId);
+
+
+
+            return View();
+        }
+    }
+}
