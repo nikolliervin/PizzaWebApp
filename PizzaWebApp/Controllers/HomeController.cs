fd1aec59d8b79faf35dee2f941e0a09002fe6ec4
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-11-25 19:27:15 +0100

    Updating Amount if CartItem Exists

--- PizzaWebApp/Controllers/HomeController.cs
+++ PizzaWebApp/Controllers/HomeController.cs
@@ -11,66 +11,89 @@ using System.Security.Claims;
 
 namespace PizzaWebApp.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDBContext _db;
-        public HomeController(ILogger<HomeController> logger, ApplicationDBContext db)
-        {
-            _logger = logger;
-            _db = db;
-        }
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDBContext _db;
+		public HomeController(ILogger<HomeController> logger, ApplicationDBContext db)
+		{
+			_logger = logger;
+			_db = db;
+		}
 
-        public IActionResult Index()
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            IEnumerable<Pizza> objList = _db.Pizza;
-            var count = _db.Cart.
-                Where(c => c.UserId == Convert.ToInt32(userId)).Count().ToString();
-            if (count == "0")
-                ViewBag.CartNumber = "";
-            else
-                ViewBag.CartNumber = $"{count}";
-            return View(objList);
-        }
-
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult AddToCart(int? id)
-        {
-            var PizzaItem = _db.Pizza.Find(id);
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            CartItems item = new CartItems
-            {
-                Amount = 1,
-                Pizza = PizzaItem,
-                PizzaName = PizzaItem.Name,
-                PizzaIngredients = PizzaItem.Ingredients,
-                PizzaPrice = PizzaItem.Price,
-                CartItemTotal = PizzaItem.Price,
-                UserId = Convert.ToInt32(userId)
-
-            };
-
-            if (ModelState.IsValid)
-            {
-                _db.Cart.Add(item);
-                _db.SaveChanges();
-            }
-
-            return RedirectToAction("Index");
-        }
+		public IActionResult Index()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			IEnumerable<Pizza> objList = _db.Pizza;
+			var count = _db.Cart.
+				Where(c => c.UserId == Convert.ToInt32(userId)).Count().ToString();
+			if (count == "0")
+				ViewBag.CartNumber = "";
+			else
+				ViewBag.CartNumber = $"{count}";
+			return View(objList);
+		}
 
 
 
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
-    }
+		[Authorize]
+		[HttpPost]
+		public IActionResult AddToCart(int? id)
+		{
+
+			var PizzaItem = _db.Pizza.Find(id);
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			CartItems item = new CartItems
+			{
+				Amount = 1,
+				Pizza = PizzaItem,
+				PizzaName = PizzaItem.Name,
+				PizzaIngredients = PizzaItem.Ingredients,
+				PizzaPrice = PizzaItem.Price,
+				CartItemTotal = PizzaItem.Price,
+				UserId = Convert.ToInt32(userId)
+
+			};
+			var objectExists = _db.Cart.Where(u => u.UserId == Convert.ToInt32(userId)).Select(o => o.PizzaName);
+			if (objectExists != null)
+			{
+				var cartItemId = Convert.ToInt32(_db.Cart.Where(p => p.PizzaName == item.PizzaName).Select(c => c.Id).ToList()[0]);
+				var modifyAmount = new CartItems
+				{
+					Id = cartItemId,
+					Pizza = PizzaItem,
+					PizzaName = PizzaItem.Name,
+					PizzaIngredients = PizzaItem.Ingredients,
+					PizzaPrice = PizzaItem.Price,
+					CartItemTotal = PizzaItem.Price,
+					UserId = Convert.ToInt32(userId),
+					Amount = item.Amount + 1
+				};
+				_db.Cart.Update(modifyAmount);
+				_db.SaveChanges();
+			}
+			else
+			{
+				if (ModelState.IsValid)
+				{
+					_db.Cart.Add(item);
+					_db.SaveChanges();
+				}
+			}
+
+
+
+			return RedirectToAction("Index");
+		}
+
+
+
+
+	}
 }
