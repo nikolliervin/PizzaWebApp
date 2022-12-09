5665688dc36dcacb55e4804fd3bfe7b7f0bacc83
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-12-09 15:43:38 +0100

    Removed ToDo

--- PizzaWebApp/Controllers/AccountController.cs
+++ PizzaWebApp/Controllers/AccountController.cs
@@ -6,122 +6,122 @@ using System.Threading.Tasks;
 
 namespace PizzaWebApp.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly ApplicationDBContext _db;
-        private readonly IdentityAppContext _identity;
-        private UserManager<AppUser> userManager { get; }
+	public class AccountController : Controller
+	{
+		private readonly ApplicationDBContext _db;
+		private readonly IdentityAppContext _identity;
+		private UserManager<AppUser> userManager { get; }
 
-        private SignInManager<AppUser> signInManager { get; }
-        public AccountController(
-        ApplicationDBContext db,
-        UserManager<AppUser> _userManager,
-        SignInManager<AppUser> _signInManager,
-        IdentityAppContext identity)
-        {
-            _db = db;
-            userManager = _userManager;
-            signInManager = _signInManager;
-            _identity = identity;
-        }
+		private SignInManager<AppUser> signInManager { get; }
+		public AccountController(
+		ApplicationDBContext db,
+		UserManager<AppUser> _userManager,
+		SignInManager<AppUser> _signInManager,
+		IdentityAppContext identity)
+		{
+			_db = db;
+			userManager = _userManager;
+			signInManager = _signInManager;
+			_identity = identity;
+		}
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-        public IActionResult Register()
-        {
-            return View();
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
 
-        }
-
-
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> Register(AppUser NewUser)
-        {
-            try
-            {
-                ViewBag.Message = "A user with that username exists or the password was not in the correct format";
-
-                AppUser user = await userManager.FindByEmailAsync(NewUser.Email);
-                if (user == null)
-                {
-                    user = new AppUser();
-                    user.Email = NewUser.Email;
-                    user.UserName = NewUser.UserName;
+		}
 
 
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		public async Task<IActionResult> Register(AppUser NewUser)
+		{
+			try
+			{
+				ViewBag.Message = "A user with that username exists or the password was not in the correct format";
 
-                    IdentityResult result = await userManager.CreateAsync(user, NewUser.PasswordHash);
-                    if (result.Succeeded)
-                    {
-                        await signInManager.SignInAsync(user, isPersistent: false);
-                        return RedirectToAction("Index", "Home");
-
-                    }
-                    else
-                        ViewBag.Error = "Account creating failed, please try again";
+				AppUser user = await userManager.FindByEmailAsync(NewUser.Email);
+				if (user == null)
+				{
+					user = new AppUser();
+					user.Email = NewUser.Email;
+					user.UserName = NewUser.UserName;
 
 
 
+					IdentityResult result = await userManager.CreateAsync(user, NewUser.PasswordHash);
+					if (result.Succeeded)
+					{
+						await signInManager.SignInAsync(user, isPersistent: false);
+						return RedirectToAction("Index", "Home");
 
-                }
-            }
-
-            catch (Exception ex)
-            {
-
-                ViewBag.Message = ex.Message;
-            }
-
-            return View();
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await signInManager.SignOutAsync();
-            return RedirectToAction("Login");
-        }
-
-        //TODO:Refactor the messy method
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> Login(AppUser user)
-        {
-            var login = new AppUser
-            {
-                UserName = user.UserName,
-                PasswordHash = user.PasswordHash,
-            };
-
-            if (!User.Identity.IsAuthenticated)
-            {
-                var result = await userManager.FindByNameAsync(login.UserName);
-                if (result == null)
-                {
-                    ViewBag.Error = "An account with that username was not found";
-
-                    return View();
-
-                }
-                else
-                {
-                    var loginResult = await signInManager.PasswordSignInAsync(login.UserName, login.PasswordHash, false, false);
-                    if (!loginResult.Succeeded)
-                    {
-                        ViewBag.Error = "Your username or password was incorrect";
-                        return View();
-                    }
-
-                }
-
-            }
+					}
+					else
+						ViewBag.Error = "Account creating failed, please try again";
 
 
-            return RedirectToAction("Index", "Home");
 
-        }
 
-    }
+				}
+			}
+
+			catch (Exception ex)
+			{
+
+				ViewBag.Message = ex.Message;
+			}
+
+			return View();
+		}
+
+		public async Task<IActionResult> Logout()
+		{
+			await signInManager.SignOutAsync();
+			return RedirectToAction("Login");
+		}
+
+
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		public async Task<IActionResult> Login(AppUser user)
+		{
+			var login = new AppUser
+			{
+				UserName = user.UserName,
+				PasswordHash = user.PasswordHash,
+			};
+
+			if (!User.Identity.IsAuthenticated)
+			{
+				var result = await userManager.FindByNameAsync(login.UserName);
+				if (result == null)
+				{
+					ViewBag.Error = "An account with that username was not found";
+
+					return View();
+
+				}
+				else
+				{
+					var loginResult = await signInManager.PasswordSignInAsync(login.UserName, login.PasswordHash, false, false);
+					if (!loginResult.Succeeded)
+					{
+						ViewBag.Error = "Your username or password was incorrect";
+						return View();
+					}
+
+				}
+
+			}
+
+
+			return RedirectToAction("Index", "Home");
+
+		}
+
+	}
 }
--- PizzaWebApp/Controllers/AdminController.cs
+++ PizzaWebApp/Controllers/AdminController.cs
@@ -11,180 +11,180 @@ using System.Threading.Tasks;
 
 namespace PizzaWebApp.Controllers
 {
-    [Authorize(Roles = "Admin")]
-    public class AdminController : Controller
-    {
-        private readonly ApplicationDBContext _db;
-        private readonly IdentityAppContext _identity;
-        private UserManager<AppUser> userManager { get; }
+	[Authorize(Roles = "Admin")]
+	public class AdminController : Controller
+	{
+		private readonly ApplicationDBContext _db;
+		private readonly IdentityAppContext _identity;
+		private UserManager<AppUser> userManager { get; }
 
-        private SignInManager<AppUser> signInManager { get; }
+		private SignInManager<AppUser> signInManager { get; }
 
-        private RoleManager<AppRole> roleManager { get; }
-        public AdminController(
-        ApplicationDBContext db,
-        UserManager<AppUser> _userManager,
-        SignInManager<AppUser> _signInManager,
-        IdentityAppContext identity,
-        RoleManager<AppRole> roles)
-        {
-            _db = db;
-            userManager = _userManager;
-            signInManager = _signInManager;
-            _identity = identity;
-            roleManager = roles;
-        }
+		private RoleManager<AppRole> roleManager { get; }
+		public AdminController(
+		ApplicationDBContext db,
+		UserManager<AppUser> _userManager,
+		SignInManager<AppUser> _signInManager,
+		IdentityAppContext identity,
+		RoleManager<AppRole> roles)
+		{
+			_db = db;
+			userManager = _userManager;
+			signInManager = _signInManager;
+			_identity = identity;
+			roleManager = roles;
+		}
 
-        public IActionResult Index()
-        {
-            var todayDate = DateTime.Now.ToString("yyyy-MM-dd");
-            List<string> orderDates = _db.Orders.Select(o => o.Date.ToString()).ToList();
-            List<string> bookingDates = _db.Bookings.Select(o => o.Date).ToList();
-            List<double> revenue = _db.Orders.Select(o => o.Price).ToList();
-            List<double> todayRevenue =
-                _db.Orders
-                .Where(o => o.Date
-                .ToString()
-                .Contains(todayDate))
-                .Select(o => o.Price)
-                .ToList();
-            var totalProducts = _db.Pizza.Select(p => p.Id).ToList().Count;
+		public IActionResult Index()
+		{
+			var todayDate = DateTime.Now.ToString("yyyy-MM-dd");
+			List<string> orderDates = _db.Orders.Select(o => o.Date.ToString()).ToList();
+			List<string> bookingDates = _db.Bookings.Select(o => o.Date).ToList();
+			List<double> revenue = _db.Orders.Select(o => o.Price).ToList();
+			List<double> todayRevenue =
+				_db.Orders
+				.Where(o => o.Date
+				.ToString()
+				.Contains(todayDate))
+				.Select(o => o.Price)
+				.ToList();
+			var totalProducts = _db.Pizza.Select(p => p.Id).ToList().Count;
 
-            ViewBag.TodayOrders = CountOrderWhere(orderDates, todayDate);
-            ViewBag.TotalOrders = orderDates.Count;
-            ViewBag.TotalUsers = _identity.Users.Select(o => o.Id).ToList().Count;
-            ViewBag.TableBookings = bookingDates.Count;
-            ViewBag.TodayBookings = CountOrderWhere(bookingDates, todayDate);
-            ViewBag.Revenue = revenue.Sum().ToString("0.00");
-            ViewBag.RevenueToday = todayRevenue.Sum().ToString("0.00");
-            ViewBag.TotalProducts = totalProducts;
-
-
-            return View();
-        }
-
-        public int CountOrderWhere(List<string> orders, string date)
-        {
-            var counter = 0;
-            foreach (var item in orders)
-            {
-                if (item.Contains(date))
-                    counter++;
-
-            }
-
-            return counter;
-        }
-
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        public List<OrderDisplayViewModel> UserOrder()
-        {
-
-            var query = (from s in _db.ShippingDetails
-                         join o in _db.Orders on
-                       s.Id equals o.ShippingId
-                         select new OrderDisplayViewModel
-                         {
-                             Name = s.Name,
-                             Surname = s.Surname,
-                             PhoneNumber = s.PhoneNumber,
-                             Street = s.Street,
-                             OrderDesc = o.OrderDesc,
-                             Price = o.Price,
-                             Date = o.Date,
-                         }).ToList();
-            return query;
-        }
-        public IActionResult Orders()
-        {
-            var ordersVM = UserOrder();
-            return View(ordersVM);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(AppUser user)
-        {
-            //@TODO:Simplyfy the method
-            var Message = "Your admin username or admin password was not correct";
-            var adminUser = new AppUser
-            {
-                UserName = user.UserName,
-                PasswordHash = user.PasswordHash
-            };
-
-            var userId = _identity.Users.Where(u => u.UserName == adminUser.UserName).Select(s => s.Id).ToList()[0];
-            var role = _identity.UserRoles.Where(s => s.UserId == userId).Select(s => s.RoleId);
-
-            try
-            {
-                var roleId = role.ToList()[0];
-
-            }
-            catch (Exception e)
-            {
-                ViewBag.Message = "This account is not an admin account";
-                return View();
-            }
+			ViewBag.TodayOrders = CountOrderWhere(orderDates, todayDate);
+			ViewBag.TotalOrders = orderDates.Count;
+			ViewBag.TotalUsers = _identity.Users.Select(o => o.Id).ToList().Count;
+			ViewBag.TableBookings = bookingDates.Count;
+			ViewBag.TodayBookings = CountOrderWhere(bookingDates, todayDate);
+			ViewBag.Revenue = revenue.Sum().ToString("0.00");
+			ViewBag.RevenueToday = todayRevenue.Sum().ToString("0.00");
+			ViewBag.TotalProducts = totalProducts;
 
 
+			return View();
+		}
+
+		public int CountOrderWhere(List<string> orders, string date)
+		{
+			var counter = 0;
+			foreach (var item in orders)
+			{
+				if (item.Contains(date))
+					counter++;
+
+			}
+
+			return counter;
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public List<OrderDisplayViewModel> UserOrder()
+		{
+
+			var query = (from s in _db.ShippingDetails
+						 join o in _db.Orders on
+					   s.Id equals o.ShippingId
+						 select new OrderDisplayViewModel
+						 {
+							 Name = s.Name,
+							 Surname = s.Surname,
+							 PhoneNumber = s.PhoneNumber,
+							 Street = s.Street,
+							 OrderDesc = o.OrderDesc,
+							 Price = o.Price,
+							 Date = o.Date,
+						 }).ToList();
+			return query;
+		}
+		public IActionResult Orders()
+		{
+			var ordersVM = UserOrder();
+			return View(ordersVM);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(AppUser user)
+		{
+
+			var Message = "Your admin username or admin password was not correct";
+			var adminUser = new AppUser
+			{
+				UserName = user.UserName,
+				PasswordHash = user.PasswordHash
+			};
+
+			var userId = _identity.Users.Where(u => u.UserName == adminUser.UserName).Select(s => s.Id).ToList()[0];
+			var role = _identity.UserRoles.Where(s => s.UserId == userId).Select(s => s.RoleId);
+
+			try
+			{
+				var roleId = role.ToList()[0];
+
+			}
+			catch (Exception e)
+			{
+				ViewBag.Message = "This account is not an admin account";
+				return View();
+			}
 
 
 
 
-            if (role.ToList()[0] == 1)
-            {
-                var loginResult = await signInManager.PasswordSignInAsync(adminUser.UserName, adminUser.PasswordHash, false, false);
-                if (!loginResult.Succeeded)
-                {
-                    ViewBag.Message = Message;
-                    return View();
-                }
-                else
-                {
-                    return View("Index");
-                }
-            }
-            else
-            {
-                ViewBag.Message = Message;
-                return View();
-
-            }
 
 
-        }
+			if (role.ToList()[0] == 1)
+			{
+				var loginResult = await signInManager.PasswordSignInAsync(adminUser.UserName, adminUser.PasswordHash, false, false);
+				if (!loginResult.Succeeded)
+				{
+					ViewBag.Message = Message;
+					return View();
+				}
+				else
+				{
+					return View("Index");
+				}
+			}
+			else
+			{
+				ViewBag.Message = Message;
+				return View();
 
-        public IActionResult Bookings()
-        {
-            IEnumerable<Bookings> Bookings = _db.Bookings;
-            return View(Bookings);
-        }
-
-        public IActionResult Contacts()
-        {
-            IEnumerable<Contact> Contacts = _db.Contact;
-            return View(Contacts);
-        }
-
-        public IActionResult Users()
-        {
-            IEnumerable<AppUser> Users = _identity.Users;
-            return View(Users);
-        }
+			}
 
 
-        public async Task<IActionResult> DeleteUser(int? id)
-        {
-            var user = _identity.Users.Find(id);
-            await userManager.DeleteAsync(user);
+		}
 
-            return RedirectToAction("Users");
-        }
+		public IActionResult Bookings()
+		{
+			IEnumerable<Bookings> Bookings = _db.Bookings;
+			return View(Bookings);
+		}
 
-    }
+		public IActionResult Contacts()
+		{
+			IEnumerable<Contact> Contacts = _db.Contact;
+			return View(Contacts);
+		}
+
+		public IActionResult Users()
+		{
+			IEnumerable<AppUser> Users = _identity.Users;
+			return View(Users);
+		}
+
+
+		public async Task<IActionResult> DeleteUser(int? id)
+		{
+			var user = _identity.Users.Find(id);
+			await userManager.DeleteAsync(user);
+
+			return RedirectToAction("Users");
+		}
+
+	}
 }
